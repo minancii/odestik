b8a206a5f81d82851b70d5c9f144a2f6dac12d3e
minancii <mehmetinanciusa@gmail.com>
2025-12-10 01:04:55 -0500

    a

---
+++ js/config.js
@@ -0,0 +1,11 @@
+/**
+ * Config.js
+ * REPLACE THESE VALUES with your own from the Supabase Dashboard.
+ */
+const SUPABASE_URL = 'https://ohvkafgcdbywimzfzcbt.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9odmthZmdjZGJ5d2ltemZ6Y2J0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUzMzMyMzUsImV4cCI6MjA4MDkwOTIzNX0.gi81e-goNy5Lx1ZhrlTjUWWAQvXtFmogPQZwxlV42DI';
+
+window.APP_CONFIG = {
+    supabaseUrl: SUPABASE_URL,
+    supabaseKey: SUPABASE_KEY
+};
